406010809ad59f3748d44983a30a250aea2c6811
Cooper55555 <milanvelthaak1@gmail.com>
2024-11-13 23:43:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,197 @@
+function home() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+    document.getElementById("ChaseCards").style.display = "none";
+    document.getElementById("homelogos").style.display = "flex";
+}
+
+function PaldeaEvolved() {
+    document.getElementById("card1").style.display = "block";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+    document.getElementById("ChaseCards").style.display = "block";
+    document.getElementById("homelogos").style.display = "none";
+}
+
+function CrownZenith() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "block";
+    document.getElementById("card3").style.display = "block";
+    document.getElementById("card4").style.display = "block";
+    document.getElementById("card5").style.display = "block";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "block";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+    document.getElementById("ChaseCards").style.display = "block";
+    document.getElementById("homelogos").style.display = "none";
+}
+
+function ParadoxRift() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "block";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+    document.getElementById("ChaseCards").style.display = "block";
+    document.getElementById("homelogos").style.display = "none";
+}
+
+function next() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "block";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "block";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+}
+
+function previous1() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "block";
+    document.getElementById("card3").style.display = "block";
+    document.getElementById("card4").style.display = "block";
+    document.getElementById("card5").style.display = "block";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "block";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+}
+
+function next1() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "block";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "block";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+}
+
+function previous2() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "block";
+    document.getElementById("card3").style.display = "block";
+    document.getElementById("card4").style.display = "block";
+    document.getElementById("card5").style.display = "block";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "block";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+}
+
+function next2() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+}
+
+function previous3() {
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+    document.getElementById("card4").style.display = "none";
+    document.getElementById("card5").style.display = "none";
+    document.getElementById("card6").style.display = "none";
+    document.getElementById("card7").style.display = "none";
+    document.getElementById("card8").style.display = "none";
+    document.getElementById("card9").style.display = "none";
+    document.getElementById("card10").style.display = "none";
+    document.getElementById("card11").style.display = "none";
+    document.getElementById("card12").style.display = "none";
+    document.getElementById("arrowbuttons").style.display = "none";
+    document.getElementById("arrowbuttons1").style.display = "none";
+    document.getElementById("arrowbuttons2").style.display = "none";
+    document.getElementById("arrowbuttons3").style.display = "none";
+}
